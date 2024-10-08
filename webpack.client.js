38996d8a10d0e84c0f7323710960e2c7e38d3b56
Tran Hoai Phong <thp080203@mail.com>
2024-10-09 00:40:31 +0700

    Refactor build scripts for development and production environments

--- webpack.client.js
+++ webpack.client.js
@@ -1,39 +1,42 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/client/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'script.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.ico$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'favicon.ico'
+module.exports = (env) => {
+  const isProduction = env.production === true
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/client/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'script.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.ico$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'favicon.ico'
+          }
         }
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.css'
-    })
-  ],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'style.css'
+      })
+    ],
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    }
   }
 }
--- webpack.server.js
+++ webpack.server.js
@@ -1,28 +1,31 @@
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/server/index.tsx',
-  target: 'node',
-  externals: [nodeExternals()],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'server.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  node: {
-    __dirname: false
+module.exports = (env) => {
+  const isProduction = env.production === true
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/server/index.tsx',
+    target: 'node',
+    externals: [nodeExternals()],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'server.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    node: {
+      __dirname: false
+    }
   }
 }
